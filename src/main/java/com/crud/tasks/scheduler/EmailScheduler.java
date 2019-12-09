14b3d372b3237914c9dd10e3e4476820def81a04
Nikoguj <Nikoguj@gmail.com>
2019-12-09 20:59:59 +0100

    23.4

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,42 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: Onec a day email";
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "Currently is database you got: " + size + taskOrTasks(size),
+                ""));
+    }
+
+    private String taskOrTasks(long size) {
+        if(size == 1) {
+            return "task";
+        }else {
+            return "tasks";
+        }
+    }
+}
